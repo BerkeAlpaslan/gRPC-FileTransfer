b4dc772d1d1f429924bfe5513ff28baecb889b9c
Berke Alpaslan <alpaslanberke@gmail.com>
2025-07-08 19:56:49 +0300

    gRPC File Transfer

---
+++ GrpcFileTransfer/GrpcClient/Program.cs
@@ -0,0 +1,48 @@
+﻿using Grpc.Net.Client;
+using GrpcFileTransferDownloadClient;
+using System;
+
+namespace GrpcDownloadClient
+{
+    class Program
+    {
+        static async Task Main (string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("http://localhost:5277");
+            var downloadClient = new FileService.FileServiceClient(channel);
+
+            CancellationTokenSource cancellationTokenSource = new();
+            int count = 0;
+            decimal chunkSize = 0;
+
+            string downloaPath = @"C:\Users\berke\Documents\gRPC_FileTransfer\GrpcFileTransfer\GrpcClient\Downloads";
+
+            var fileInfo = new GrpcFileTransferDownloadClient.FileInfo
+            {
+                FileName = "chromedriver",
+                FileExtension = ".exe"
+            };
+
+            FileStream fileStream = null;
+
+            var download = downloadClient.FileDownload(fileInfo);
+            
+            while (await download.ResponseStream.MoveNext(cancellationTokenSource.Token))
+            {
+                if (count++ == 0)
+                {
+                    fileStream = new FileStream(@$"{downloaPath}/{download.ResponseStream.Current.Info.FileName}{download.ResponseStream.Current.Info.FileExtension}", FileMode.CreateNew);
+                    fileStream.SetLength(download.ResponseStream.Current.FileSize);
+                }
+
+                var buffer = download.ResponseStream.Current.Buffer.ToByteArray();
+                await fileStream.WriteAsync(buffer, 0, download.ResponseStream.Current.ReadedByte);
+                Console.WriteLine($"{Math.Round((chunkSize += download.ResponseStream.Current.ReadedByte * 100) / download.ResponseStream.Current.FileSize)}%");
+            }
+
+            Console.WriteLine("Download Completed!");
+            await fileStream.DisposeAsync();
+
+        }
+    }
+}
---
+++ GrpcFileTransfer/GrpcUploadClient/Program.cs
@@ -0,0 +1,39 @@
+﻿using Google.Protobuf;
+using Grpc.Net.Client;
+using GrpcFileTransferUploadClient;
+using System.Threading.Tasks;
+
+namespace GrpcUploadClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("http://localhost:5277");
+            var uploadClient = new FileService.FileServiceClient(channel);
+
+            string file = @"C:\Users\berke\Documents\Test\chromedriver.exe";
+
+            decimal chunkSize = 0;
+
+            FileStream fileStream = new(file, FileMode.Open);
+            var bytesContent = new BytesContent
+            {
+                FileSize = fileStream.Length,
+                ReadedByte = 0,
+                Info = new GrpcFileTransferUploadClient.FileInfo { FileName = Path.GetFileNameWithoutExtension(fileStream.Name), FileExtension = Path.GetExtension(fileStream.Name) }
+            };
+
+            var upload = uploadClient.FileUpload();
+            byte[] buffer = new byte[2048];
+
+            while((bytesContent.ReadedByte = await fileStream.ReadAsync(buffer, 0, buffer.Length)) > 0)
+            {
+                bytesContent.Buffer = ByteString.CopyFrom(buffer);
+                await upload.RequestStream.WriteAsync(bytesContent);
+            }
+            await upload.RequestStream.CompleteAsync();
+            var response = await upload.ResponseAsync;
+        }
+    }
+}
